79433a141d2bf6bc6572a4973410e31be51f40bb
sibanarayan10 <sibanarayan014@gmail.com>
2025-04-02 21:17:08 +0530

    update:vite config && backend unhandled route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import { connectMongoDB } from "./src/Connect/connectMongo.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 dotenv.config({ path: "./.env" });
@@ -49,6 +50,11 @@ process.on("SIGINT", async () => {
 });
 app.get("/", (req, res) => {
   res.send("server is listening..");
+});
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "frontend", "vite-project", "dist", "index.html")
+  );
 });
 // router set-up
 app.use("/api/user", userRouter);
--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  preview: {
+    port: 5000,
+  },
+});
